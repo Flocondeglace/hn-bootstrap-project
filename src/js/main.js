11ff1cf6829e902588efed8113a899087ae39405
Flocondeglace <floriane.payen@gmail.com>
2026-03-26 13:49:19 +0100

    fix: tooltip

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,11 @@ import "../scss/styles.scss";
 
 // Import all of Bootstrap’s JS
 import * as bootstrap from "bootstrap";
+
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .querySelectorAll('[data-bs-toggle="tooltip"]')
+    .forEach(function (element) {
+      new bootstrap.Tooltip(element);
+    });
+});
